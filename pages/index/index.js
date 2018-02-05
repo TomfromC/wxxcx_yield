4e50c3e02d2dc2fd8f7600c2d1e2ab85f8e3c779
hht_mmtchina <haohaitaoapache@163.com>
2018-02-05 13:39:39 +0800

    修改bug

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,10 +9,10 @@ Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
-    yieldData:{}
+    yieldData: {}
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -21,31 +21,31 @@ Page({
     console.log('onLoad')
     var that = this;
     //===================== yield start======================
-      co(function *(){
-        try{
-          //调用应用实例的方法获取全局数据
-          app.getUserInfo(function (userInfo) {
-            //更新数据
-            that.setData({
-              userInfo: userInfo
-            })
-          });
-          wx.showLoading({
-            title: '加载中...',
-          })
-          var result = yield myAsyncFunc('haohaitao', 18);
-          console.log(result.data);
-          var result2 = yield myAsyncFunc2();
-          console.log(result2.data);
-          var yieldData = {firstData: result.data, secondData: result2.data};
+    co(function* () {
+      try {
+        //调用应用实例的方法获取全局数据
+        app.getUserInfo(function (userInfo) {
+          //更新数据
           that.setData({
-            yieldData: yieldData
+            userInfo: userInfo
           })
-          wx.hideLoading();
-        }catch(e){
-          console.log(e);
-        }
-      });
+        });
+        wx.showLoading({
+          title: '加载中...',
+        })
+        var result = yield myAsyncFunc('haohaitao', 18);
+        console.log(result.data);
+        var result2 = yield myAsyncFunc2();
+        console.log(result2.data);
+        var yieldData = { firstData: result.data, secondData: result2.data };
+        that.setData({
+          yieldData: yieldData
+        })
+        wx.hideLoading();
+      } catch (e) {
+        console.log(e);
+      }
+    });
     //===================== yield end  ======================
   }
 })
@@ -69,8 +69,8 @@ var myAsyncFunc = (username, age) => {
  * 使用promise构建方法,co依赖promise
  */
 var myAsyncFunc2 = () => {
-  return new Promise((resolve, reject) =>{
-    setTimeout(() =>{
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
       resolve({
         data: 'hello world'
       })
